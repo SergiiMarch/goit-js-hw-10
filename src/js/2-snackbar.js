7b15703e53533b1e0fac643e8f1e216d4f0ec352
Sergii Marchenko <marikmarchelo@gmail.com>
2024-06-28 11:09:29 +0300

    add new commit task 10/2

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -8,14 +8,36 @@ refs.formEl.addEventListener('submit', onSubmit);
 function onSubmit(e) {
   e.preventDefault();
 
-  const delay = refs.formEl.elements.delay.value;
+  const delay = Number(refs.formEl.elements.delay.value);
   const state = refs.formEl.elements.state.value;
+  console.log('✅ Fulfilled promise in:', delay);
+  console.log('State:', state);
 
-  greatePromise(delay, state)
+  createPromise(delay, state)
     .then(message => {
-      console.log('succeos');
+      console.log('✅ Fulfilled promise in:', message);
     })
     .catch(message => {
-      console.log('error');
+      console.log('❌ Rejected promise in:', message);
     });
 }
+
+function createPromise(delay, state) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'Fulfilled') {
+        resolve(`${delay}ms`);
+      } else {
+        reject(`${delay}ms`);
+      }
+    }, delay);
+  });
+}
+
+// createPromise(1000, 'Fulfilled')
+//   .then(message => console.log('✅ Success:', message))
+//   .catch(message => console.log('❌ Error:', message));
+
+// createPromise(1000, 'Rejected')
+//   .then(message => console.log('✅ Success:', message))
+//   .catch(message => console.log('❌ Error:', message));
